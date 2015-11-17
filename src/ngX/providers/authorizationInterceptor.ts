6297d9eaa12b65f68ae694671d8cfa10632c795f
Quinntyne Brown <quinntynebrown@gmail.com>
2015-11-17 06:27:13 -0500

    feat(security): auth interceptor and formEncode

---
+++ src/ngX/providers/authorizationInterceptor.ts
@@ -0,0 +1,32 @@
+﻿module ngX {
+
+    "use strict";
+
+    class AuthorizationInterceptor {
+
+        constructor(private securityStore:any) {
+
+        }
+
+        public static instance = (securityStore: any) => {
+            return new AuthorizationInterceptor(securityStore);
+        }
+
+        public request = (config) => {
+            if (this.securityStore.token) {
+                config.headers.Authorization = "Bearer " + this.securityStore.token;
+            }
+
+            return config;
+        }
+    }
+
+    angular.module("ngX")
+        .factory("authorizationInterceptor", ["securityStore", AuthorizationInterceptor.instance])
+        .config([
+            "$httpProvider", ($httpProvider) => {
+                $httpProvider.interceptors.push("authorizationInterceptor");
+            }
+        ]);
+
+}
---
+++ src/ngX/providers/formEncode.ts
@@ -0,0 +1,15 @@
+﻿﻿module ngX {
+
+    "use strict";
+
+    var formEncode = (data) => {
+        var pairs = [];
+        for (var name in data) {
+            pairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+        }
+        return pairs.join('&').replace(/%20/g, '+');
+    };
+
+    angular.module("ngX").value("formEncode", formEncode);
+
+}
