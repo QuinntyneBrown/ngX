49f37088db5f69d8bbb5d626711c8f6756d53654
Quinntyne Brown <quinntynebrown@gmail.com>
2015-12-31 23:34:18 -0500

    feat(invokeAsync)

--- dist/ngX.js
+++ dist/ngX.js
@@ -796,6 +796,40 @@ var ngX;
 
 var ngX;
 (function (ngX) {
+    "use strict";
+    var dispatcher = null;
+    var $q = null;
+    var invokeAsync = function (options) {
+        if (!dispatcher) {
+            dispatcher = angular.element(document.body).injector().get("dispatcher");
+        }
+        if (!$q) {
+            $q = angular.element(document.body).injector().get("$q");
+        }
+        if (angular.isFunction(options)) {
+            options = { action: options };
+        }
+        ;
+        var deferred = $q.defer();
+        var actionId = options.action();
+        var listenerId = dispatcher.addListener({
+            actionType: "CHANGE",
+            callback: function (callbackOptions) {
+                if (actionId === callbackOptions.id) {
+                    dispatcher.removeListener({ id: listenerId });
+                    deferred.resolve();
+                }
+            }
+        });
+        return deferred.promise;
+    };
+    angular.module("ngX").value("invokeAsync", invokeAsync);
+})(ngX || (ngX = {}));
+
+//# sourceMappingURL=invokeAsync.js.map
+
+var ngX;
+(function (ngX) {
     var LocalStorageManager = (function () {
         function LocalStorageManager($window) {
             var _this = this;
